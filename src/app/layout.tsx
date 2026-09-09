e49a8a1f594772620cec1b5c8f447bc7a8e786d7
Sujay wali <sujaywali@gmail.com>
2026-09-09 13:55:43 +0530

    Wrap children with AuthGate in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>
         <Navbar />
-        {children}
+        <AuthGate>{children}</AuthGate>
       </body>
     </html>
   );
